c8cce7507e78b8e329fe4709ec704509222881bf
Cedrick Embestro <cedrick13wew@gmail.com>
2023-09-04 22:25:01 -0800

    Update Text

--- add.php
+++ add.php
@@ -45,7 +45,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="student.php">CRUD </a>
+                <a class="navbar-brand" href="student.php">ADD </a>
             </div>
      
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
