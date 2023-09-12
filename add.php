021b2d14552a8797cf8715dceb0c054a250a3207
Cedrick Embestro <cedrick13wew@gmail.com>
2023-09-11 23:10:35 -0800

    Change Small Letter

--- add.php
+++ add.php
@@ -45,7 +45,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="student.php">ADD Data</a>
+                <a class="navbar-brand" href="student.php">Add Data</a>
             </div>
      
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
